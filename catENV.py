287e36fbb3bd005fd0ddf622190c1201205aa823
Termonoid <termonoid@yandex.ru>
2020-11-02 16:52:11 +0300

    Удалены неиспользуемые переменные в resize_image.

--- catENV.py
+++ catENV.py
@@ -133,9 +133,7 @@ def writeTo(text, target):
 def resize_image(input_image_path, output_image_path, size):
     try:
         original_image = Image.open(input_image_path)
-        width, height = original_image.size
         resized_image = original_image.resize(size)
-        width, height = resized_image.size
         resized_image.save(output_image_path)
         return "ok"
     except Exception as e:
