4e6571286cda5fce00b1214f29b374b3291165c3
catware1 <52168620+catware1@users.noreply.github.com>
2020-12-25 08:25:37 +0300

    Update catENV.py
    Names to lowercase

--- catENV.py
+++ catENV.py
@@ -34,7 +34,7 @@ def convertint(uptime): # Converting seconds to verbal notation, by Catware & Ca
     minutes = int(minutes - (hours * 60 + days * 24 * 60));
     return str(days) + ' дней ' + str(hours) + ' часов ' + str(minutes) + ' минут ' 
 
-def HostToIp(host):
+def hosttoip(host):
     return gethostbyname(host)
 
 def percent(frst, scnd):
@@ -45,7 +45,7 @@ def percent(frst, scnd):
 def deunix(integer):
     return datetime.datetime.fromtimestamp(integer).strftime('%Y %m %d %H %M %S').split(" ")
 
-def Geocode(address):
+def geocode(address):
     address = address.replace(" ", "+")
     json1 = convertjson(Get("https://nominatim.openstreetmap.org/search?q="+ address + "&format=geojson"))
     json2 = json1["features"][0]["geometry"]["coordinates"]
@@ -60,15 +60,15 @@ def translate(text, lang):
     result = translator.translate(text, dest = str(lang))
     return result.text
 
-def RandomInt(first, second):
+def randomint(first, second):
     return randd.randint(first, second)
 
-def PlusWrite(text, target):
+def pluswrite(text, target):
     file = open(str(target), 'a', encoding='utf-8')
     file.write(str(text))
     file.close()
 
-def Get(url): # A get requests
+def get(url): # A get requests
     try:
         return get(url).text
         log("Issued Get() with URL {}".format(url))
@@ -76,17 +76,17 @@ def Get(url): # A get requests
         log("Error in Get(). " + str(e))
         return None
 
-def ShortUrl(url):
+def shorturl(url):
     return Get("https://clck.ru/--?url=" + url)
 
-def InstallPackage(text): # Install Python Package (PIP)
+def installpackage(text): # Install Python Package (PIP)
     a = CallSystem("pip install " + str(text) + ' --user')
     if 'error' not in str(a).lower():
         return 'success'
     else:
         return 'error'
 
-def ReadFF(file): # Read From File
+def readff(file): # Read From File
     try:
         Ff = open(file, 'r', encoding='UTF-8')
         Contents = Ff.read()
@@ -95,48 +95,48 @@ def ReadFF(file): # Read From File
     except:
         return None
 
-def CallSystem(command): # Call system shell
+def callsystem(command): # Call system shell
     return str(check_output(str(command), shell=False))
 
-def Run(file): # Запустить питоновский скрипт в рамках DEFa (а нахуя это кстати я не знаю.)
-    exec(ReadFF(str(file)))
+def run(file): # Запустить питоновский скрипт в рамках DEFa (а нахуя это кстати я не знаю.)
+    exec(readff(str(file)))
 
-def Download(url, fn): # Download a file from any URL
+def download(url, fn): # Download a file from any URL
     f = open(fn, 'wb')
     f.write(get(url).content)
     f.close()
 
-def Similar(first, second): # Similar strings
+def similar(first, second): # Similar strings
     if not len(first) == len(second):
         return False
     if len(first) - sum(l1==l2 for l1, l2 in zip(first, second)) > 3:
         return False
     return True
 
-def TextToBits(text, encoding='utf-8', errors='surrogatepass'): # Text to 101010010100101
+def texttobits(text, encoding='utf-8', errors='surrogatepass'): # Text to 101010010100101
     bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
     return bits.zfill(8 * ((len(bits) + 7) // 8))
 
-def TextFromBits(bits, encoding='utf-8', errors='surrogatepass'): # Text from 10101001010101
+def textfrombits(bits, encoding='utf-8', errors='surrogatepass'): # Text from 10101001010101
     try:
         n = int(bits, 2)
         return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
     except Exception:
         return 'error'
 
-def CreateFile(name): # Create a file
+def createfile(name): # Create a file
     k = name
     f = open(str(k), 'w')
     f.close()
 
-def Reverse(s): # Reverse text (Text -> txeT)
+def reverse(s): # Reverse text (Text -> txeT)
     return s[::-1]
 
-def RandomLetter():
+def randomletter():
     letters = ['q', 'Q', 'w', 'W', 'e', 'E', 'r', 'R', 't', 'T', 'y', 'Y', 'u', 'U', 'i', 'I', 'o', 'O', 'p', 'P', 'a', 'A', 's', 'S', 'd', 'D', 'f', 'F', 'g', 'G', 'h', 'H', 'j', 'J', 'k', 'K', 'l', 'L', 'z', 'Z', 'x', 'X', 'c', 'C', 'v', 'V', 'b', 'B', 'n', 'N', 'm', 'M', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
     return randd.choice(letters)
 
-def writeTo(text, target):
+def writeto(text, target):
     file = open(str(target), 'w', encoding='utf-8')
     file.write(str(text))
     file.close()
